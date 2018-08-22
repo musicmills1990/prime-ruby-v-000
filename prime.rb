0df9de1b554cdfadb0c3fa5644921038ca47a4c2
Marc Mills <mdmills1990@gmail.com>
2018-08-22 23:50:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,14 @@
 def prime?(num)
-  array = (2..num).to_a
-  array.detect do |number|
-    !((num % number).is_a? Integer || 1)
+  array = (2..num.to_f).to_a
+  array.collect do |number|
+    if (num / number).is_a? Integer &&  !((num / number) == 1)
+      false
+    else
+      true
     end
+    end  
 end
+
+
+
+#so if your array is 2..23 because your number is 23, then 23/2 is NOT an integer 23/3 is NOT an integer
